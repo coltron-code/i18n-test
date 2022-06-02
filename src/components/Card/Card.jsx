9cd0833e224b55d15a5393c4c9f1f25b83607d1f
Diego Gimbernat <gimbernat13@gmail.com>
2022-06-02 13:31:38 -0500

    created card comp

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import ArrowRightSVG from '../../assets/right-arrow.png';
+
+const StyledCard = styled('div')`
+  width: 100%;
+  border-radius: 24px;
+  transition: all 0.2s;
+  border: 1px solid rgba(0, 0, 0, 0.06);
+  box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.02);
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
+const StyledCardInner = styled('div')`
+  padding: 24px 22px;
+  display: grid;
+  gap: 5px;
+
+  /* background-color: red; */
+`;
+const StyledCardTitle = styled('div')`
+  font-size: 24px;
+`;
+const StyledCardDescription = styled('div')`
+  font-size: 16px;
+`;
+const StyledCardImage = styled('img')`
+ margin-left: auto;
+ display: block;
+`;
+
+function Card({ title, description, link }) {
+  return (
+    <StyledCard>
+      <StyledCardInner>
+        <StyledCardTitle>{title}</StyledCardTitle>
+        <StyledCardDescription>{description}</StyledCardDescription>
+        <StyledCardImage src={ArrowRightSVG} alt="" />
+      </StyledCardInner>
+    </StyledCard>
+  );
+}
+
+export default Card;
---
+++ src/pages/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Translate from '@docusaurus/Translate';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import styled from 'styled-components';
+import styles from './index.module.css';
+import Card from '../components/Card/Card';
+
+const StyledFaqCardGrid = styled('div')`
+  display: grid;
+  /* grid-template-rows: minmax(220px, 1fr ); */
+  /* grid-template-columns: minmax(0, 3fr) minmax(0, 3fr) minmax(0, 3fr); */
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+
+  align-items: center;
+  gap: 24px;
+  justify-items: center;
+  align-items: center;
+`;
+const StyledContainer = styled('div')`
+  width: 80%;
+  margin: 0 auto;
+`;
+
+export default function Hojme() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <StyledContainer>
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <StyledFaqCardGrid>
+          <Card title="Set your Primary Name" description="Associate an ENS name with your Ethereum wallet." />
+          <Card title="Set your Primary Name" description="Associate an ENS name with your Ethereum wallet." />
+          <Card title="Set your Primary Name" description="Associate an ENS name with your Ethereum wallet." />
+        </StyledFaqCardGrid>
+      </StyledContainer>
+
+      {/* <NavigationV2 /> */}
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
